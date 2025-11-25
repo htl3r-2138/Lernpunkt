f476b2087b436baa3a0468e71f4380393060aecb
Linus Bittner <2130@htl.rennweg.at>
2025-11-25 12:52:03 +0100

    DB init.sql fixed

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,9 @@
 CREATE DATABASE LP_db;
+USE LP_db;
 CREATE TABLE user (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(100),
   password VARCHAR(100)
 );
+
+INSERT INTO user (username, password) VALUES ("test", "1234");
