51844b3d5e4fe4adf72579e62f4a567d20357e16
HTL3R-2138 <2138@htl.rennweg.at>
2026-01-11 12:06:08 +0100

    database changes - new attributes, new tables need and is_in

--- database/init.sql
+++ database/init.sql
@@ -5,11 +5,14 @@ USE LP_db;
 
 CREATE TABLE Tutor
 (
-    PK_Tutor_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Name        VARCHAR(100)        NOT NULL,
-    Surname     VARCHAR(100)        NOT NULL,
-    Password    VARCHAR(255)        NOT NULL,
-    Email       VARCHAR(255) UNIQUE NOT NULL
+    PK_Tutor_ID  INT AUTO_INCREMENT PRIMARY KEY,
+    Name         VARCHAR(100)        NOT NULL,
+    Surname      VARCHAR(100)        NOT NULL,
+    Password     VARCHAR(255)        NOT NULL,
+    Email        VARCHAR(255) UNIQUE NOT NULL,
+    Grade        INT                 NOT NULL,
+    CHECK (Grade BETWEEN 1 AND 5),
+    PricePerHour DECIMAL(4, 2)
 );
 
 CREATE TABLE Student
@@ -18,39 +21,51 @@ CREATE TABLE Student
     Name          VARCHAR(100)        NOT NULL,
     Surname       VARCHAR(100)        NOT NULL,
     Password      VARCHAR(255)        NOT NULL,
-    Email         VARCHAR(255) UNIQUE NOT NULL
+    Email         VARCHAR(255) UNIQUE NOT NULL,
+    Grade         INT                 NOT NULL,
+    CHECK (Grade BETWEEN 1 AND 5)
 );
 
 CREATE TABLE Subject
 (
     PK_Subject_ID INT AUTO_INCREMENT PRIMARY KEY,
-    Description   VARCHAR(255) NOT NULL,
-    Grade         INT          NOT NULL
+    Description   VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Booking
 (
     PK_Booking_ID    INT AUTO_INCREMENT PRIMARY KEY,
-    Date             DATETIME     NOT NULL,
+    Date             DATE         NOT NULL,
+    Start            TIME         NOT NULL,
+    End              TIME         NOT NULL,
     MeetUp           VARCHAR(255) NOT NULL,
     Description      VARCHAR(255) NOT NULL,
     Notes            VARCHAR(255),
-    Hours            INT          NOT NULL,
+    isAccepted       BOOLEAN,
+    isDone           BOOLEAN,
+    Stars            INT,
+    CHECK (Stars BETWEEN 1 AND 5),
     FK_PK_Student_ID INT,
-    FOREIGN KEY (FK_PK_Student_ID) REFERENCES Student (PK_Student_ID
-                                                      )
+    FOREIGN KEY (FK_PK_Student_ID) REFERENCES Student (PK_Student_ID)
 );
 
 CREATE TABLE contain
 (
     FK_PK_Booking_ID INT NOT NULL,
     FK_PK_Subject_ID INT NOT NULL,
-    Price            DECIMAL(5, 2),
     PRIMARY KEY (FK_PK_Booking_ID, FK_PK_Subject_ID),
     FOREIGN KEY (FK_PK_Booking_ID) REFERENCES Booking (PK_Booking_ID),
     FOREIGN KEY (FK_PK_Subject_ID) REFERENCES Subject (PK_Subject_ID)
 );
 
+CREATE TABLE need
+(
+    FK_PK_Student_ID   INT NOT NULL,
+    FK_PK_Subject_ID INT NOT NULL,
+    PRIMARY KEY (FK_PK_Student_ID, FK_PK_Subject_ID),
+    FOREIGN KEY (FK_PK_Student_ID) REFERENCES Student (PK_Student_ID),
+    FOREIGN KEY (FK_PK_Subject_ID) REFERENCES Subject (PK_Subject_ID)
+);
 
 CREATE TABLE offer
 (
@@ -61,6 +76,14 @@ CREATE TABLE offer
     FOREIGN KEY (FK_PK_Subject_ID) REFERENCES Subject (PK_Subject_ID)
 );
 
+CREATE TABLE is_in
+(
+    FK_PK_Tutor_ID   INT NOT NULL,
+    FK_PK_Booking_ID INT NOT NULL,
+    PRIMARY KEY (FK_PK_Tutor_ID, FK_PK_Booking_ID),
+    FOREIGN KEY (FK_PK_Tutor_ID) REFERENCES Tutor (PK_Tutor_ID),
+    FOREIGN KEY (FK_PK_Booking_ID) REFERENCES Booking (PK_Booking_ID)
+);
 
 CREATE TABLE Admin
 (
