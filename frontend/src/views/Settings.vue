aeb801394b4d15b89fe575025bb8fd84566a5d38
Linus Bittner <2130@htl.rennweg.at>
2026-01-12 14:43:16 +0100

    settings skeleton for email an pw change done

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -2,9 +2,10 @@
   <div class="wrapper">
     <nav>
       <div class="return">
-        <button>
-          
-        </button>
+        <div class="back" @click="exitSettings">
+          <img src="@/assets/settings/X.svg" alt="exit settings" />
+          <span>Exit</span>
+        </div>
       </div>
       <div class="navTitle">
         <h1>Settings</h1>
@@ -18,10 +19,7 @@
           <img src="@/assets/settings/logout.svg" alt="Logout" />
           <span>Logout</span>
         </div>
-        <div class="back" @click="exitSettings">
-          <img src="@/assets/settings/X.svg" alt="exit settings">
-          <span>Exit</span>
-        </div>
+
         <ConfirmModal
           v-if="showConfirm"
           title="Confirm logout"
@@ -31,14 +29,74 @@
         />
       </div>
     </nav>
+    <main>
+      <div class="email">
+        <form @submit.prevent="changeEmail">
+          <TextField name="currEmail" label="current Email Address" required />
+          <TextField name="newEmail" label="new Email Address" required />
+          <button
+            class="login-btn"
+            @click="handleLogin"
+            @mouseenter="hoverEnter"
+            @mouseleave="hoverLeave"
+          >
+            set new Email
+          </button>
+        </form>
+      </div>
+      <div class="password">
+        <form @submit.prevent>
+          <PasswordField
+            name="currentPassword"
+            label="current Password"
+            type="password"
+            required
+          />
+
+          <PasswordField
+            name="newPassword"
+            label="new Password"
+            type="password"
+            required
+          />
+
+          <PasswordField
+            name="repeatNewPassword"
+            label="repeat new Password"
+            type="password"
+            required
+          />
+
+          <button
+            class="login-btn"
+            @mouseenter="hoverEnter"
+            @mouseleave="hoverLeave"
+          >
+            set new password
+          </button>
+        </form>
+      </div>
+    </main>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { gsap } from "gsap";
 import { useRouter } from "vue-router";
 import { logout } from "@/services/auth";
 import ConfirmModal from "@/components/ConfirmModal.vue";
+import TextField from "@/components/authentification/TextField.vue";
+import PasswordField from "@/components/authentification/PasswordField.vue";
+
+// Hover-Effekte
+function hoverEnter(event) {
+  gsap.to(event.target, { scale: 1.1, duration: 0.2, ease: "power1.in" });
+}
+
+function hoverLeave(event) {
+  gsap.to(event.target, { scale: 1, duration: 0.2, ease: "power1.out" });
+}
 
 const router = useRouter();
 const showConfirm = ref(false);
@@ -74,7 +132,7 @@ const exitSettings = () => {
 nav {
   display: flex;
   justify-content: space-between;
-  padding: 10px;
+  padding: 2rem;
 }
 .accountFeatures {
   display: flex;
@@ -86,5 +144,23 @@ nav {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+main{
+  display: flex;
+  justify-content: center;
+  gap: 10%;
+}
+
+.login-btn {
+  padding: 15px 30px;
+  background: #7e52fc;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+  transition: box-shadow 0.2s;
+  margin-top: 5%;
 }
 </style>
